fc497afd200cf72a33426b10037e553e9ab7c91a
liangzhuangzhuang <895641390@qq.com>
2022-05-25 15:36:06 +0800

    deploy

---
+++ precache-manifest.112e9fea3dd366da082f73834b43a381.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6b8091bd12124f6c37740cacb6e49b3",
+    "url": "/morney-react-website/index.html"
+  },
+  {
+    "revision": "37906d95bbbdcbec10f1",
+    "url": "/morney-react-website/static/css/main.787b3cc7.chunk.css"
+  },
+  {
+    "revision": "6c153bc362bff2315ae8",
+    "url": "/morney-react-website/static/js/2.0ffe286f.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/morney-react-website/static/js/2.0ffe286f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37906d95bbbdcbec10f1",
+    "url": "/morney-react-website/static/js/main.bde3df39.chunk.js"
+  },
+  {
+    "revision": "5794ca38023fa78af21b",
+    "url": "/morney-react-website/static/js/runtime-main.e48f6080.js"
+  }
+]);
